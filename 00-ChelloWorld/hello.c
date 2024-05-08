5a0c96c7a68ee6cf30e1979ee0ebb3f37dfb7249
Enzokm15 <enzomarmartin2016@gmail.com>
2024-05-07 22:47:47 -0300

    elimanr hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -4,23 +4,8 @@ int main()
 {
     printf("hello world.\n");
 
-    FILE* archivo;
-    //abrimos el archivo
-    archivo=fopen("output.txt","w");
     
-    if (archivo==NULL){
-        printf("error al abrir archivo.\n");
-
-    }
-    
-    fprintf(archivo, "Ejemplo de un archivo en C.\n");
-    //cerramos el archivo 
-    fclose(archivo);
-    
-
     return 0;
-
-
 
 }
 
